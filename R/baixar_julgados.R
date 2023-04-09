ee35cbfcfd71d729a82c8961188c56a28f409996
NunesDev04 <filipenunesdev@outlook.com>
2023-04-09 11:33:40 -0300

    09.04.23

--- R/baixar_julgados.R
+++ R/baixar_julgados.R
@@ -1,5 +1,9 @@
 library(tjsp)
 library(tidyverse)
+library(lubridate)
+library(JurisMiner)
+
+?tjsp
 
 classe <- "417"
 assuntos <- "3607,3608,5885,10523,5894,5895,5896,5897,5898,5899,5900,5901,10987"
@@ -32,6 +36,7 @@ dados <- ler_dados_cposg(arquivos = arquivos)
 partes <- tjsp_ler_partes(arquivos)
 
 movimentacao <- ler_movimentacao_cposg(arquivos)
+
 ############################
 n_proc_comarca <- cjsg |> 
   count(comarca)
@@ -43,8 +48,6 @@ proc_p_relator <- cjsg |>
   count(relator)
 ############################
 
-library(JurisMiner)
-
 movimentacao <- movimentacao |> 
   tempo_movimentacao()
 
@@ -53,8 +56,44 @@ tempo_processo <- movimentacao |>
   summarise(tempo = max(decorrencia_acumulada))
 
 relator_ementa <- cjsg |> 
-  select(relator, ementa)
+  select(data_julgamento,processo, relator, orgao_julgador, ementa)
+
+relator_ementa <- relator_ementa |> 
+  distinct()
+
+decisoes <- tjsp_ler_decisoes_cposg(diretorio = "data_raw/cposg")
+
+decisoes <- decisoes |> 
+  distinct()
+
+decisoes2 <- decisoes |> 
+  select(dispositivo)
+
+decisoes3 <- decisoes |> 
+  select(processo, dispositivo)
+
+## agora preciso juntar decises com relator ementa - NAO CONSEGUI AINDA
+
+base_final <- merge(relator_ementa, decisoes2) # deu certo mas deu errado
+
+base_final <- base_final |> 
+  distinct()
+
+base_final2 <- relator_ementa |> 
+  inner_join(relator_ementa, join_by(processo)) |> 
+  inner_join(decisoes3, join_by(processo))
+  
 
 
+  #full_join(relator_ementa, decisoes, by="processo" )
   
+  #anti_join(relator_ementa, decisoes, by="processo" )
   
+  #inner_join(relator_ementa, decisoes, by="processo" )
+  
+  #semi_join(relator_ementa, decisoes, by="processo" )
+
+#base_final2 <- base_final |> 
+  #mutate(decisao = tjsp_classificar_sentenca())
+#rm(base_final2)
+
